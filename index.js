ab42de80eac38e9657252c39d9d11175297d0483
anna-hy-zhang <anna.hy.zhang@gmail.com>
2021-07-15 21:35:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-function getTweets() {
-  fetch('https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev')
-  .then(response => response.json())
-  .then(data => console.log(data));
-}
 
-getTweets();
+fetch('https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev')
+.then(response => response.json())
+.then(data => console.log(data));
+
 
 
 
