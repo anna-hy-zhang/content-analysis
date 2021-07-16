53075bc93ca437360fb924ec1aa7e16782d88fbd
anna-hy-zhang <anna.hy.zhang@gmail.com>
2021-07-15 21:39:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-fetch('https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev')
+fetch('https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev'--header 'Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAFfdRgEAAAAA1DtfpgGzB9iuoifRo%2BKhpcPj9%2B0%3DUVZP7FRJOqBh8lKFfls9GJCAOSoaKCkl6JBUugQJ43s8XUAmSW')
 .then(response => response.json())
 .then(data => console.log(data));
 
